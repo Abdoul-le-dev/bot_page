1a651731820354276b73c6d8c006729b83aa604e
Abdoul-le-dev <abdoulledev@gmail.com>
2026-06-08 11:05:57 +0200

    suite

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
 Route::get('/login', [AuthController::class, 'loginform'])->name('login');
-Route::post('login', [AuthController::class, 'login']);
+Route::post('login', [AuthController::class, 'login'])->name('admin.login');
 Route::post('logout', [AuthController::class, 'logout'])->name('logout');
 
 
