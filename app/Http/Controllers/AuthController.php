36888a53847e0c50c7a3a3e73ee5b07964d4a80d
Abdoul-le-dev <abdoulledev@gmail.com>
2026-06-08 11:19:58 +0200

    suite

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,24 +4,21 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 
 class AuthController extends Controller
 {
-    public function up(): void
-{
-   
 
-    DB::table('users')->insert([
+    public function loginForm()
+    {   
+        DB::table('users')->insert([
         'email' => 'admin@fiacregoldtrade.com',
         'password' => Hash::make('Fdksignal2026@'),
         'created_at' => now(),
         'updated_at' => now(),
     ]);
-}
-    public function loginForm()
-    {   
-        up();
         if (Auth::check() && in_array(Auth::user())) {
             return $this->redirectAfterLogin(Auth::user());
         }
