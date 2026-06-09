5303ea069730259a60cafc71ebbac615502a0d42
Abdoul-le-dev <abdoulledev@gmail.com>
2026-06-09 13:12:09 +0200

    suite

--- public/js/chat_render.js
+++ public/js/chat_render.js
@@ -153,7 +153,7 @@ function renderReplyQuote(msg) {
     </div>`
 }
 
-const MEDIA_BASE = 'https://52.90.21.131'
+const MEDIA_BASE = 'http://52.90.21.131:8000'
 
 function renderMediaContent(msg) {
   if (!msg.media_url) return ''
